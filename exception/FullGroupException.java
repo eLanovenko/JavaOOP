4939561ea7bd8096e056c11f47149a7797f57cb1
eLanovenko <63804302+eLanovenko@users.noreply.github.com>
2021-03-11 15:05:55 +0200

    Create FullGroupException.java

---
+++ exception/FullGroupException.java
@@ -0,0 +1,16 @@
+package homework3.exception;
+
+public class FullGroupException extends Exception{
+
+	public FullGroupException() {
+		super();
+	
+	}
+
+	public FullGroupException(String message) {
+		super(message);
+		
+	}
+	
+
+}
